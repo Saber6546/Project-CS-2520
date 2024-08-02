d86de529b3b6ece312652e074ee804152de4fbe3
Saber6546 <125634743+Saber6546@users.noreply.github.com>
2024-08-01 17:07:03 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Colors
+BACKGROUND_COLOR = "#2C2C2C"  # Dark Gray background
+BUTTON_COLOR = "#4CAF50"      # Green buttons
+TEXT_COLOR = "#FFFFFF"        # White text
+
+# Font Sizes
+FONT_SIZE_LABEL = 12
+FONT_SIZE_ENTRY = 12
+FONT_SIZE_BUTTON = 12
+
+# Sizes
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 500
